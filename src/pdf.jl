df4ce6ea6185fecd7de55132ca34b4c8b0373ac2
Adam <adamgoast360@outlook.com>
2025-07-01 14:37:00 +0200

    fix

--- src/pdf.jl
+++ src/pdf.jl
@@ -1,9 +1,8 @@
 using NNlib
 
-function logpdf_MOG(x::Float64, 
-                        μ::Vector{Float64}, 
-                        σ::Vector{Float64}, 
-                        logw::Vector{Float64})
-    L = @. logw - log(σ) - 0.5 * log(2π) - (x - μ)^2 / (2 * σ^2)
-    return logsumexp(L)
+function logpdf_MOG(x::AbstractArray{Float64}, 
+                        μ::AbstractArray{Float64}, 
+                        σ::AbstractArray{Float64}, 
+                        logw::AbstractArray{Float64})
+    return logsumexp((@. logw - log(σ) - 0.5 * log(2π) - (x - μ)^2 / (2 * σ^2)), dims=1)
 end
