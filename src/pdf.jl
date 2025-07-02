05056107836667b2815a7782c7ce4ce2efc78805
Adam <adamgoast360@outlook.com>
2025-07-02 10:57:47 +0200

    transformation-3d-matrix

--- src/pdf.jl
+++ src/pdf.jl
@@ -2,7 +2,7 @@ using NNlib
 
 function logpdf_MOG(x::AbstractArray{Float64}, 
                         μ::AbstractArray{Float64}, 
-                        σ::AbstractArray{Float64}, 
+                        σ::AbstractArray{Float64},
                         logw::AbstractArray{Float64})
     return logsumexp((@. logw - log(σ) - 0.5 * log(2π) - (x - μ)^2 / (2 * σ^2)), dims=1)
 end
