708b37f0dbbf2848a785d74c2977b724deaaab70
Alice <alice.stenbeck@icloud.com>
2025-07-03 10:18:56 +0200

    hej

---
+++ Random transform molecule.jl
@@ -0,0 +1,17 @@
+using LinearAlgebra  # För att kunna använda `qr` och `det`
+
+function transform_molecule(X::AbstractMatrix{<:Real})
+    @assert size(X, 1) == 3
+
+    Q, _ = qr(randn(3, 3)) # Slumpar en 3*3 matris baserat på normalfördelning. Representeras av rand N. Men för att få en riktig rotation och inte bara kaos siffror måste vi använda QR dekomposition, vilket qr(...) gör. Den delar upp matrisen i 2 delar: Q – en ortogonal matris (tänk: snurr) R – en övre triangulär matris (tänk: skala & sträck. Q,_ innebär strunta i R och tänk på Q. 
+    if det(Q) < 0 # Determinanten är ett tal som säger om en matris skalar, vänder eller kollapsar rymden. Determinanten av Q ska vara +1 men om den blir -1 måste vi göra den till +1
+        Q[:,1] .= -Q[:,1]
+    end
+    R = Q # För att kalla Q för R som står för rotation 
+
+    X_rotated = R * X # Först roterar vi varje punkt
+    t = randn(3)
+    X_transformed = X_rotated .+ t # Sedan translationen vi varje punkt genom att elementvis lägga till translationen. 
+
+    return X_transformed # rotation + translation kallas för transformation 
+end
